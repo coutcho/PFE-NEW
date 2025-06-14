0f1d6c907e0044c83a2225769a9dde6924248ffa
coutcho <rayan.adjina2002@gmail.com>
2025-06-14 21:49:41 +0100

    edit db.js

--- backend/db.js
+++ backend/db.js
@@ -2,19 +2,13 @@
 import pkg from 'pg';
 import dotenv from 'dotenv';
 
-dotenv.config({path: '../.env'});
+dotenv.config(); // Don't specify a path, use default `.env` in root
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
-
-console.log("DB password type:", typeof process.env.DB_PASSWORD);
-console.log("DB password value:", process.env.DB_PASSWORD);
 
 export default pool;
